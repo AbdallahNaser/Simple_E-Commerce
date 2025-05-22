f80217cb5a9de39995d9b69e4441ce85a3f3137f
AbdallahNaser <abdallahnser555@gmail.com>
2025-05-22 15:27:58 +0300

    product crud using view set

--- product/api/urls.py
+++ product/api/urls.py
@@ -1,8 +1,16 @@
 # api/urls.py
-from django.urls import path
+from django.urls import path,include
 from .views import *
+from rest_framework.routers import DefaultRouter
+
+
+
+router = DefaultRouter()
+router.register(r'productsV', ProductViewSet, basename='product')
 
 urlpatterns = [
+    path('', include(router.urls)),
+
     path('products/', product_list_create, name='product-list-create'),
     path('products/update/<int:pk>/', ProductUpdateView.as_view(), name='product-update'),
     path('products/<int:pk>/', ProductDetailUpdateDeleteView.as_view(), name='product-detail-update-delete'),
--- product/api/views.py
+++ product/api/views.py
@@ -6,7 +6,18 @@ from rest_framework import status,generics
 from ..models import Product
 from .serializers import ProductSerializer
 from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
 
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.filter(status=True)  # Only show active products
+    serializer_class = ProductSerializer
+
+    def perform_destroy(self, instance):
+        # Soft delete: set status to False instead of deleting
+        instance.status = False
+        instance.save()
 
 @api_view(['GET', 'POST'])
 def product_list_create(request):
@@ -21,6 +32,7 @@ def product_list_create(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
 
 class ProductUpdateView(APIView):
     def put(self, request, pk):
